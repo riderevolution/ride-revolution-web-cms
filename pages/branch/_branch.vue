61d6fc670426499f6cc6b93488ee7fbffce1cfcd
BiLos <bstamariadesignblue@gmail.com>
2019-07-30 22:10:19 +0800

    started the col_two_alt

--- pages/branch/_branch.vue
+++ pages/branch/_branch.vue
@@ -9,7 +9,7 @@
             </section>
             <section id="content" class="alternate">
                 <div class="header_selector">
-                    <select class="default_selector">
+                    <select class="default_select">
                         <option value="greenbelt">Greenbelt</option>
                         <option value="shangri-la-plaza">Shangri-la Plaza</option>
                     </select>
@@ -19,11 +19,11 @@
                         <div class="filter">
                             <h2 class="action_highlight"><span>&#9679;</span> Juan Dela Cruz</h2>
                             <div class="filter_selector">
-                                <select class="default_selector">
+                                <select class="default_select">
                                     <option value="1">May 1, 2019</option>
                                 </select>
                                 <div class="divider">&mdash;</div>
-                                <select class="default_selector">
+                                <select class="default_select">
                                     <option value="2">May 5, 2019</option>
                                 </select>
                             </div>
@@ -36,6 +36,42 @@
                                 </div>
                                 <div class="total_count">{{ data.count }}</div>
                                 <div class="total_text">{{ data.label }}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="cms_col_alt_two">
+                        <div class="wrapper">
+                            <div class="header_wrapper">
+                                <div class="header_title">Merchandise Inventory</div>
+                                <div class="header_buttons">
+                                    <nuxt-link class="action_btn" to="/">Audit</nuxt-link>
+                                    <nuxt-link class="action_btn" to="/">Order</nuxt-link>
+                                </div>
+                                <input class="default_text search" type="text" placeholder="Search merchandise...">
+                            </div>
+                            <div class="list_wrapper">
+                                <div class="list" v-for="(data, key) in res.merchs">
+                                    <img src="/icons/bev-icon.svg" />
+                                    <div class="list_title">{{ data.label }}</div>
+                                    <div class="list_count">{{ data.count }}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="wrapper">
+                            <div class="header_wrapper">
+                                <div class="header_title">Food &amp; Beverage Inventory</div>
+                                <div class="header_buttons">
+                                    <nuxt-link class="action_btn" to="/">Audit</nuxt-link>
+                                    <nuxt-link class="action_btn" to="/">Order</nuxt-link>
+                                </div>
+                                <input class="default_text search" type="text" placeholder="Search food and beverage...">
+                            </div>
+                            <div class="list_wrapper">
+                                <div class="list" v-for="(data, key) in res.bevs">
+                                    <img src="/icons/bev-icon.svg" />
+                                    <div class="list_title">{{ data.label }}</div>
+                                    <div class="list_count">{{ data.count }}</div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -92,6 +128,66 @@
                             label: 'Total Revenue from Packages',
                             count: '₱47,830',
                             imgSrc: '/icons/total-revenue-icon.svg'
+                        }
+                    ],
+                    merchs: [
+                        {
+                            label: 'A 2XU M Shirt Black Php2,500.00',
+                            count: 5
+                        },
+                        {
+                            label: 'B 2XU M Shirt Scarlet Scarlet Scarlet',
+                            count: 5
+                        },
+                        {
+                            label: 'A 2XU W Compression 3/4 BLK DRF',
+                            count: 30
+                        },
+                        {
+                            label: 'B 2XU M Shirt Black Php2,500.00',
+                            count: 40
+                        },
+                        {
+                            label: 'C 2XU M Shirt Scarlet Scarlet Scarlet',
+                            count: 50
+                        },
+                        {
+                            label: 'D 2XU W Compression 3/4 BLK DRF',
+                            count: 60
+                        },
+                        {
+                            label: 'E 2XU M Shirt Scarlet Scarlet Scarlet',
+                            count: 70
+                        }
+                    ],
+                    bevs: [
+                        {
+                            label: 'A BAI Drink',
+                            count: 1
+                        },
+                        {
+                            label: 'B Hope in a Bottle',
+                            count: 5
+                        },
+                        {
+                            label: 'A Justin’s Classic Peanut Butter',
+                            count: 200
+                        },
+                        {
+                            label: 'B BAI Drink',
+                            count: 100
+                        },
+                        {
+                            label: 'C Hope in a Bottle',
+                            count: 25
+                        },
+                        {
+                            label: 'D Avocado Pure Nectar',
+                            count: 50
+                        },
+                        {
+                            label: 'E Cashew Milk Pure Nectar',
+                            count: 20
                         }
                     ]
                 }
