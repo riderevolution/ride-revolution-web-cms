2fde13beb0cc8b55be8654b225b3c103d8486520
bilos <bstamariadesignblue@gmail.com>
2019-09-09 21:13:22 +0800

    done with the inventory products section

---
+++ pages/retail/inventory/index.vue
@@ -0,0 +1,290 @@
+<template>
+    <div class="content">
+        <div id="admin" class="cms_dashboard">
+            <section id="top_content" class="table" v-if="loaded">
+                <div class="action_wrapper">
+                    <h1 class="header_title">Inventory</h1>
+                    <div class="actions">
+                        <!-- <div class="total">Total: {{ totalItems((res.productVariants) ? (res.classPackages ? 1 : res.productVariants.total ) : 1) }}</div> -->
+                        <div class="toggler">
+                            <div :class="`status ${(status == 1) ? 'active' : ''}`" @click="toggleOnOff(1)">Activated</div>
+                            <div :class="`status ${(status == 0) ? 'active' : ''}`" @click="toggleOnOff(0)">Deactivated</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="action_buttons" v-if="package_status == 1">
+                    <nuxt-link :to="`/${lastRoute}/product-categories`" class="action_btn"><svg xmlns="http://www.w3.org/2000/svg" width="17.016" height="17.016" viewBox="0 0 17.016 17.016"><defs></defs><g transform="translate(-553 -381)"><circle class="add" cx="8.508" cy="8.508" r="8.508" transform="translate(553 381)"/><g transform="translate(558.955 386.955)"><line class="add_sign" y2="5.233" transform="translate(2.616 0)"/><line class="add_sign" x2="5.233" transform="translate(0 2.616)"/></g></g></svg>Edit Categories</nuxt-link>
+                    <nuxt-link :to="`${$route.path}/class-packages/create`" class="action_btn margin"><svg xmlns="http://www.w3.org/2000/svg" width="17.016" height="17.016" viewBox="0 0 17.016 17.016"><defs></defs><g transform="translate(-553 -381)"><circle class="add" cx="8.508" cy="8.508" r="8.508" transform="translate(553 381)"/><g transform="translate(558.955 386.955)"><line class="add_sign" y2="5.233" transform="translate(2.616 0)"/><line class="add_sign" x2="5.233" transform="translate(0 2.616)"/></g></g></svg>Physical Count</nuxt-link>
+                    <nuxt-link :to="`/${lastRoute}/purchase-order`" class="action_btn margin"><svg xmlns="http://www.w3.org/2000/svg" width="17.016" height="17.016" viewBox="0 0 17.016 17.016"><defs></defs><g transform="translate(-553 -381)"><circle class="add" cx="8.508" cy="8.508" r="8.508" transform="translate(553 381)"/><g transform="translate(558.955 386.955)"><line class="add_sign" y2="5.233" transform="translate(2.616 0)"/><line class="add_sign" x2="5.233" transform="translate(0 2.616)"/></g></g></svg>New Purchase Order</nuxt-link>
+                </div>
+                <div class="action_buttons" v-if="package_status == 2">
+                    <nuxt-link :to="`/${lastRoute}/promotion/create`" class="action_btn"><svg xmlns="http://www.w3.org/2000/svg" width="17.016" height="17.016" viewBox="0 0 17.016 17.016"><defs></defs><g transform="translate(-553 -381)"><circle class="add" cx="8.508" cy="8.508" r="8.508" transform="translate(553 381)"/><g transform="translate(558.955 386.955)"><line class="add_sign" y2="5.233" transform="translate(2.616 0)"/><line class="add_sign" x2="5.233" transform="translate(0 2.616)"/></g></g></svg>Add a Promotion</nuxt-link>
+                </div>
+                <div class="filter_wrapper">
+                    <form class="filter_flex" id="filter" method="post" @submit.prevent="submissionSuccess('products')" v-if="package_status == 1">
+                        <div class="form_group">
+                            <label for="category_id">Category</label>
+                            <select class="default_select alternate" name="category_id">
+                                <option value="" selected>All Categories</option>
+                                <option :value="category.id" v-for="(category, key) in categories" :key="key">{{ category.name }}</option>
+                            </select>
+                        </div>
+                        <div class="form_group margin">
+                            <label for="supplier_id">Supplier</label>
+                            <select class="default_select alternate" name="supplier_id">
+                                <option value="" selected>All Suppliers</option>
+                                <option :value="supplier.id" v-for="(supplier, key) in suppliers" :key="key">{{ supplier.name }}</option>
+                            </select>
+                        </div>
+                        <div class="form_group margin">
+                            <label for="studio_id">Studio</label>
+                            <select class="default_select alternate" name="studio_id">
+                                <option value="" selected>All Studios</option>
+                                <option :value="studio.id" v-for="(studio, key) in studios" :key="key">{{ studio.name }}</option>
+                            </select>
+                        </div>
+                        <div class="form_group margin">
+                            <label for="q">Find a Product</label>
+                            <input type="text" name="q" autocomplete="off" placeholder="Search for a product" class="default_text search_alternate">
+                        </div>
+                        <button type="submit" name="button" class="action_btn alternate margin">Search</button>
+                    </form>
+                    <form class="filter_flex" id="filter" method="post" @submit.prevent="submissionSuccess('promotions')" v-if="package_status == 2">
+                        <div class="form_group">
+                            <label for="q">Find a Promo</label>
+                            <input type="text" name="q" autocomplete="off" placeholder="Search for a promo" class="default_text search_alternate">
+                        </div>
+                        <button type="submit" name="button" class="action_btn alternate margin">Search</button>
+                    </form>
+                </div>
+            </section>
+            <section id="content" v-if="loaded">
+                <div class="cms_table_toggler">
+                    <div :class="`status ${(package_status == 1) ? 'active' : ''}`" @click="togglePackages(1)">Products</div>
+                    <div :class="`status ${(package_status == 2) ? 'active' : ''}`" @click="togglePackages(2)">Promotions</div>
+                    <div :class="`status ${(package_status == 3) ? 'active' : ''}`" @click="togglePackages(3)">Gift Cards</div>
+                </div>
+                <table class="cms_table" v-if="package_status == 1">
+                    <thead>
+                        <tr>
+                            <th>Product Name</th>
+                            <th>SKU ID</th>
+                            <th>Category</th>
+                            <th>Studio</th>
+                            <th>Supplier</th>
+                            <th>In Stock</th>
+                            <th>Unit Price</th>
+                            <th>Sale Price</th>
+                            <th>Sellable</th>
+                        </tr>
+                    </thead>
+                    <tbody v-if="res.productVariants.data.length > 0">
+                        <tr v-for="(data, key) in res.productVariants.data" :key="key" :class="`${(data.quantity < data.reorder_point) ? 'threshold' : ''}`">
+                            <td>{{ data.variant }}</td>
+                            <td>{{ data.sku_id }}</td>
+                            <td>{{ data.product.category.name }}</td>
+                            <td>
+                                <div v-for="(studio, key) in data.product.studio_access" :key="key" v-if="data.product.studio_access.length != studios.length">
+                                    {{ studio.studio.name }} <br />
+                                </div>
+                                <div v-if="data.product.studio_access.length == studios.length">
+                                    All Studios
+                                </div>
+                            </td>
+                            <td>{{ data.product.supplier.name }}</td>
+                            <td>{{ data.quantity }}</td>
+                            <td>PHP {{ totalCount(data.unit_price) }}</td>
+                            <td>PHP {{ totalCount(data.sale_price) }}</td>
+                            <td>{{ (data.product.sellable == 1) ? 'Yes' : 'No' }}</td>
+                        </tr>
+                    </tbody>
+                    <tbody class="no_results" v-else>
+                        <tr>
+                            <td :colspan="rowCount">No Result(s) Found.</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table class="cms_table" v-if="package_status == 2">
+                    <thead>
+                        <tr>
+                            <th>Promo Name</th>
+                            <th>Discount</th>
+                            <th>Promo Code</th>
+                            <th>Start date</th>
+                            <th>End Date</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Sample</td>
+                            <td>50% Off</td>
+                            <td>SampleCode123</td>
+                            <td>{{ formatDate(new Date()) }}</td>
+                            <td>{{ formatDate(new Date()) }}</td>
+                            <td class="table_actions">
+                                <nuxt-link class="table_action_edit" :to="`${$route.path}/promotions/1/edit`">Edit</nuxt-link>
+                                <a class="table_action_cancel" @click.self="toggleStatus(1, 0, 'Deactivated')" href="javascript:void(0)" v-if="status == 1">Deactivate</a>
+                                <a class="table_action_success" @click.self="toggleStatus(1, 1, 'Activated')" href="javascript:void(0)" v-if="status == 0">Activate</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <!-- <tbody class="no_results" v-else>
+                        <tr>
+                            <td :colspan="rowCount">No Result(s) Found.</td>
+                        </tr>
+                    </tbody> -->
+                </table>
+                <!-- <pagination :apiRoute="(res.productVariants) ? (res.classPackages ? res.classPackages.path : res.productVariants.path ) : res.storeCredits.path" :current="(res.productVariants) ? (res.classPackages ? res.classPackages.current_page : res.productVariants.current_page ) : res.storeCredits.current_page" :last="(res.productVariants) ? (res.classPackages ? res.classPackages.last_page : res.productVariants.last_page ) : res.storeCredits.last_page" /> -->
+            </section>
+        </div>
+        <foot v-if="$store.state.isAuth" />
+        <transition name="fade">
+            <confirm-status v-if="$store.state.confirmStatus" ref="enabled" :status="status" :packageStatus="package_status" />
+        </transition>
+    </div>
+</template>
+
+<script>
+    import Foot from '../../../components/Foot'
+    import Pagination from '../../../components/Pagination'
+    import ConfirmStatus from '../../../components/modals/ConfirmStatus'
+    export default {
+        components: {
+            Foot,
+            Pagination,
+            ConfirmStatus,
+        },
+        data () {
+            return {
+                loaded: false,
+                lastRoute: '',
+                rowCount: 0,
+                status: 1,
+                package_status: 1,
+                res: [],
+                categories: [],
+                suppliers: [],
+                studios: [],
+                form_search: {
+                    user: ''
+                }
+            }
+        },
+        methods: {
+            formatDate (value) {
+                if (value) {
+                    return this.$moment(value).format('MMM DD, YYYY')
+                }
+            },
+            submissionSuccess (type) {
+                const me = this
+                let apiRoute = ''
+                let formData = new FormData(document.getElementById('filter'))
+                formData.append('enabled', me.status)
+                switch (type) {
+                    case 'products':
+                        apiRoute = 'api/inventory/product-variants/search'
+                        break
+                }
+                me.loader(true)
+                me.$axios.post(apiRoute, formData).then(res => {
+                    if (res.data) {
+                        me.res = res.data
+                        if (me.res.productVariants) {
+                            me.res.productVariants.data.sort((a, b) => {
+                                return a.reached_reorder_point - b.reached_reorder_point
+                            })
+                        }
+                    }
+                }).catch(err => {
+                    me.$store.state.errorList = err.response.data.errors
+                    me.$store.state.errorStatus = true
+                }).then(() => {
+                    setTimeout( () => {
+                        me.loader(false)
+                    }, 500)
+                })
+            },
+            toggleStatus (id, enabled, status) {
+                const me = this
+                me.$store.state.confirmStatus = true
+                setTimeout( () => {
+                    me.$refs.enabled.confirm.table_name = (me.res.classPackages) ? 'class_packages' : 'store_credits'
+                    me.$refs.enabled.confirm.id = id
+                    me.$refs.enabled.confirm.enabled = enabled
+                    me.$refs.enabled.confirm.status = status
+                    me.$refs.enabled.confirm.type = (me.res.classPackages) ? 'class package' : 'store credits'
+                }, 100)
+                document.body.classList.add('no_scroll')
+            },
+            toggleOnOff (value) {
+                const me = this
+                me.status = value
+                me.fetchData(value, me.package_status)
+            },
+            togglePackages (value) {
+                const me = this
+                me.package_status = value
+                me.fetchData(me.status, value)
+            },
+            fetchData (status, packageStatus) {
+                const me = this
+                let apiRoute = ''
+                switch (packageStatus) {
+                    case 1:
+                        apiRoute = `api/inventory/product-variants?enabled=${status}`
+                        break
+                    case 2:
+                        apiRoute = `api/packages/class-packages?enabled=${status}&promo=${packageStatus}`
+                        break
+                    case 3:
+                        apiRoute = `api/packages/store-credits?enabled=${status}`
+                        break
+                }
+                me.loader(true)
+                me.$axios.get(apiRoute).then(res => {
+                    if (res.data) {
+                        me.res = res.data
+                        me.loaded = true
+                        if (me.res.productVariants) {
+                            me.res.productVariants.data.sort((a, b) => {
+                                return a.reached_reorder_point - b.reached_reorder_point
+                            })
+                        }
+                    }
+                }).catch(err => {
+                    me.$store.state.errorList = err.response.data.errors
+                    me.$store.state.errorStatus = true
+                }).then(() => {
+                    me.rowCount = document.getElementsByTagName('th').length
+                    setTimeout( () => {
+                        me.loader(false)
+                    }, 300)
+                })
+                switch (packageStatus) {
+                    case 1:
+                        me.$axios.get('api/studios').then(res => {
+                            me.studios = res.data.studios
+                        })
+                        me.$axios.get('api/suppliers').then(res => {
+                            me.suppliers = res.data.suppliers.data
+                        })
+                        me.$axios.get('api/inventory/product-categories').then(res => {
+                            me.categories = res.data.productCategories
+                        })
+                        break
+                }
+
+            }
+        },
+        async mounted () {
+            const me = this
+            me.fetchData(1, 1)
+            setTimeout( () => {
+                window.scrollTo({ top: 0, behavior: 'smooth' })
+            }, 300)
+            me.lastRoute = me.$route.path.split('/')[me.$route.path.split('/').length - 2]
+        }
+    }
+</script>
