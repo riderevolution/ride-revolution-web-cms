311497cf65d5256656349901ef6696d1909b4c42
teryong <dthcrpz@gmail.com>
2019-10-29 17:30:40 +0800

    updated nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,7 @@ export default {
   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
-    baseURL: (process.env.NODE_ENV == "development") ? 'http://api.riderevolution.test' : 'https://api.riderevol.tk',
+    baseURL: (process.env.NODE_ENV == "development") ? 'http://api.riderevolution.test' : 'http://api-riderev.designbluemanila.ph',
     // baseURL: (process.env.NODE_ENV == "development") ? 'http://127.0.0.1:8000' : 'https://api.riderevol.tk',
     credentials: false
   },
