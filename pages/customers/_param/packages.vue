a07c5d9683e9a98e776193efe09c94bda5a8480b
BiLos <bstamariadesignblue@gmail.com>
2019-11-14 17:38:19 +0800

    done with the toggling overlay

--- pages/customers/_param/packages.vue
+++ pages/customers/_param/packages.vue
@@ -39,8 +39,8 @@
                     <div :class="`status ${(package_status == 2) ? 'active' : ''}`" @click="togglePackages(2)">Shared</div>
                 </div>
                 <div class="cms_table_package">
-                    <div class="table_package">
-                        <h2 class="package_title warning">50 Class Package <span>2 Days Left</span></h2>
+                    <div class="table_package" v-for="(data, key) in res" :key="key">
+                        <h2 :class="`package_title ${data.class}`">{{ data.name }} <span>{{ data.label }}</span></h2>
                         <div class="package_details">
                             <div class="package_status">
                                 <div class="box">
@@ -69,48 +69,7 @@
                             <div class="package_action">
                                 <a href="javascript:void(0)" class="action_success_btn">Book a Class</a>
                                 <div class="package_options">
-                                    <div class="option_btn" @click.self="toggledOption($event)">Options</div>
-                                    <div class="option_selector">
-                                        <a href="javascript:void(0)" class="option_link">Transfer Package</a>
-                                        <a href="javascript:void(0)" class="option_link">Share Package</a>
-                                        <a href="javascript:void(0)" class="option_link">Freeze Package</a>
-                                        <a href="javascript:void(0)" class="option_link">Print Receipt</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="table_package">
-                        <h2 class="package_title shared">10 Class Package <span>Shared with Sheena Villeta</span></h2>
-                        <div class="package_details">
-                            <div class="package_status">
-                                <div class="box">
-                                    <div class="overlay">
-                                        <p>8</p>
-                                        <label>Used</label>
-                                    </div>
-                                </div>
-                                <div class="box margin">
-                                    <div class="overlay">
-                                        <p>2</p>
-                                        <label>Available</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="package_date">
-                                <div class="date">
-                                    <p>Apr. 04, 2019 / Apr 8, 2019</p>
-                                    <label>Purchase Date / Activation Date</label>
-                                </div>
-                                <div class="date margin">
-                                    <p>Apr 8, 2020</p>
-                                    <label>Expiry date <a href="javascript:void(0)" class="expiry_btn">Edit</a></label>
-                                </div>
-                            </div>
-                            <div class="package_action">
-                                <a href="javascript:void(0)" class="action_success_btn">Book a Class</a>
-                                <div class="package_options">
-                                    <div class="option_btn" @click.self="toggledOption($event)">Options</div>
+                                    <div class="option_btn" :id="`option_${key}`" @click.self="toggledOption($event)">Options</div>
                                     <div class="option_selector">
                                         <a href="javascript:void(0)" class="option_link">Transfer Package</a>
                                         <a href="javascript:void(0)" class="option_link">Share Package</a>
@@ -137,7 +96,19 @@
         data () {
             return {
                 lastRoute: '',
-                package_status: 1
+                package_status: 1,
+                res: [
+                    {
+                        name: '50 Class Package',
+                        class: 'warning',
+                        label: '2 days left'
+                    },
+                    {
+                        name: '10 Class Package',
+                        class: 'shared',
+                        label: 'Shared with Sheena Villeta'
+                    }
+                ]
             }
         },
         methods: {
@@ -150,6 +121,21 @@
                     element.parentNode.classList.add('toggled')
                 }
             },
+            toggleOverlays (e) {
+                const me = this
+                let target = e.target
+                me.res.forEach((result, index) => {
+                    let option = document.getElementById(`option_${index}`)
+                    if (option != null) {
+                        if (option !== target && option !== target.parentNode.previousElementSibling) {
+                            if (option.parentNode.classList.contains('toggled')) {
+                                option.parentNode.classList.remove('toggled')
+                            }
+                        }
+                    }
+                })
+
+            },
             togglePackages (status) {
                 const me = this
                 return me.package_status = status
@@ -158,6 +144,12 @@
         mounted () {
             const me = this
             me.lastRoute = me.$route.path.split('/')[me.$route.path.split('/').length - 3]
+        },
+        beforeMount () {
+            document.addEventListener('click', this.toggleOverlays)
+        },
+        beforeDestroy () {
+            document.removeEventListener('click', this.toggleOverlays)
         }
     }
 </script>
