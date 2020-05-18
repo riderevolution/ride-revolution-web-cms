4cb649e4b22d10a2ad1c7b9688636e188f325aae
BiLos <bstamariadesignblue@gmail.com>
2020-05-18 11:24:06 +0800

    fixed the validation of miamges

--- components/BannerHandler.vue
+++ components/BannerHandler.vue
@@ -66,29 +66,14 @@
             }
         },
         filters: {
-            properFormat (value) {
+            properFormat: function (value) {
                 let newValue = value.split('The ')[1].split(' field')[0].split('[]')
                 if (newValue.length > 1) {
-                    let nextValue = newValue[0].split('_')
-                    if (nextValue.length > 1) {
-                        newValue = nextValue[0].charAt(0).toUpperCase() + nextValue[0].slice(1) + ' ' + nextValue[1].charAt(0).toUpperCase() + nextValue[1].slice(1)
-                    } else {
-                        newValue = newValue[0].charAt(0).toUpperCase() + newValue[0].slice(1)
-                    }
+                    newValue = newValue[0].charAt(0).toUpperCase() + newValue[0].slice(1)
                 }else {
                     newValue = value.split('The ')[1].split(' field')[0].split('_')
                     if (newValue.length > 1) {
-                        let firstValue = ''
-                        let lastValue = ''
-                        if (newValue[0] != 'co' && newValue[0] != 'pa' && newValue[0] != 'ec' && newValue[0] != 'ba') {
-                            firstValue = newValue[0].charAt(0).toUpperCase() + newValue[0].slice(1)
-                        }
-                        for (let i = 1; i < newValue.length; i++) {
-                            if (newValue[i] != 'id') {
-                                lastValue += ' ' + newValue[i].charAt(0).toUpperCase() + newValue[i].slice(1)
-                            }
-                        }
-                        newValue = firstValue + ' ' + lastValue
+                        newValue = newValue[0].charAt(0).toUpperCase() + newValue[0].slice(1) + ' ' + newValue[1].charAt(0).toUpperCase() + newValue[1].slice(1)
                     } else {
                         newValue = value.split('The ')[1].split(' field')[0].charAt(0).toUpperCase() + value.split('The ')[1].split(' field')[0].slice(1)
                     }
@@ -96,15 +81,10 @@
                 let message = value.split('The ')[1].split(' field')
                 if (message.length > 1) {
                     message = message[1]
-                    return `The ${newValue} field${message}`
                 } else {
-					if (message[0].split('file').length > 1) {
-                        message = message[0].split('file')[1]
-                        return `The ${newValue} field${message}`
-                    } else {
-                        return `The ${newValue}`
-                    }
+                    message = message[0].split('image[]')[1]
                 }
+                return `The ${newValue} field${message}`
             }
         },
         methods: {
